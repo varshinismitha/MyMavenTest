b7ce826d94ee2b2f67135b684cbd9b251e1d1256
varshinismitha <varshinismitha2005@gmail.com>
2026-05-13 09:18:39 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Varshini Smitha S 1BI23CS236" );
+    }
+}
